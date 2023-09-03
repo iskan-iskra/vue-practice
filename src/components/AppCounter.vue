816232ad3dc13888559cb419ebb2d9667fc82737
Iskander <i.shushayev@mycar.kz>
2023-09-04 00:35:17 +0600

    feat[counter]:add vuetify

---
+++ src/components/AppCounter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "AppCounter",
+
+  setup() {
+    const myCounter = ref<number>();
+
+    return { myCounter };
+  },
+});
+</script>
+
+<style scoped></style>
